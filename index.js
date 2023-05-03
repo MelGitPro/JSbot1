5fe0eac33b92b2a04aedfb1a03f367a3be8d19e6
MelGitPro <subaruphonk12@gmail.com>
2023-05-03 14:36:26 +0700

    up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new TelegramBot('6120155424:AAF-rA1BxhHAuyOMV7b3Y5KjJDChKYA7jpw', {p
 
 bot.on('message', (msg) =>{
     const ChatId = msg.chat.id;
-    if (msg.text == ('/start' || '/start@Calculatortghyperbor')) {
+    if (msg.text == ('/start' || '/start@Calculatortghyperbot')) {
         bot.sendMessage(ChatId,'Использовать бота очень просто.\n Надо ввести свой пример в чат боту и получить ответ от него.\n Пример ввода: 5+8ю. \n Также можно использовать знаки: /,*,-,%,**');
     } else if (msg.text == '/help'){
         bot.sendMessage(ChatId, 'Бот создан на Node JS. ver 2.0js.ed')
